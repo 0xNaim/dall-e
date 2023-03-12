8cc24f58f6c2a2f87fc1e0667a9eb23e6106129f
Omar Faruq <naimislam3545@gmail.com>
2023-03-13 03:31:48 +0600

    fullstack openai image generator

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import cors from "cors";
+import * as dotenv from "dotenv";
+import express from "express";
+
+import connectDB from "./mongodb/connect.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "150mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get("/", async (req, res) => {
+  res.send("Hello from OpenAI");
+});
+
+(async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port http://localhost:8080")
+    );
+  } catch (err) {
+    console.log(err);
+  }
+})();
---
+++ server/routes/postRoutes.js
@@ -0,0 +1,46 @@
+import { v2 as cloudinary } from "cloudinary";
+import * as dotenv from "dotenv";
+import express from "express";
+
+import Post from "../models/post.js";
+
+dotenv.config();
+
+const router = express.Router();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Get all posts
+router.route("/").get(async (req, res) => {
+  try {
+    const posts = await Post.find();
+
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+});
+
+// Create a post
+router.route("/").post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+
+    res.status(201).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
+
+export default router;
